9394238dacfdb5b7e16fe617eb65922258cbb22a
Erick Martínez <mail@erickfabricio.com>
2020-03-05 15:30:29 -0600

    Servidor https config keys

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 const config = {};
 
-config.port = process.env.PORT || 3001;
+config.port = process.env.PORT || 443;
 
 config.db = 'mongodb://localhost/finance';
 
-config.key = "ERICK-2020*XXIII"
+config.key = "ERICK-2020*XXIII";
+
+config.privkey = "/etc/letsencrypt/live/finance.erickfabricio.com-0001/privkey.pem";
+config.fullchain = "/etc/letsencrypt/live/finance.erickfabricio.com-0001/fullchain.pem";
 
 module.exports = config;
