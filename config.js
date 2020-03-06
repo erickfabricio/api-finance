5b301d72b952bf11fa229cd88f2f92e5476accf9
Erick Martínez <mail@erickfabricio.com>
2020-03-06 09:10:30 -0600

    Certificado letsencrypt

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const config = {};
 config.portHttp = process.env.PORT || 3001;
 config.portHttps = process.env.PORT || 3002;
 
-config.db = 'mongodb://localhost/finance';
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+config.db = 'mongodb://localhost:27017/finance';
 
 config.key = "ERICK-2020*XXIII";
 
@@ -11,5 +13,9 @@ config.key = "ERICK-2020*XXIII";
 config.keytmp = "../keys/keytmp.pem";
 config.cert = "../keys/cert.pem";
 config.passphrase = "erick2020"
+
+//Let
+config.privkey = ".../etc/letsencrypt/live/finance.erickfabricio.com-0001/keytmp.pem";
+config.fullchain = ".../etc/letsencrypt/live/finance.erickfabricio.com-0001/fullchain.pem";
 
 module.exports = config;
